8b307769821317ea89edc05c27fd0a87a8ea6880
swetha <tswetha352@gmail.com>
2023-04-08 17:01:54 +0530

    Add Components

--- App.js
+++ App.js
@@ -1,5 +1,28 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-const h1 = React.createElement('h1',{},'Hello dsjhsjghsjworld')
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(h1)
+import React from "react";
+import ReactDOM from "react-dom/client";
+//React element
+// const h1 = React.createElement("h1", { id: "heading" }, "Hello react");
+//JSX/ReactElement
+const h2 = <h2>Hii Hello React</h2>; 
+
+//React Component
+//Class Component - Old way 
+
+//Functional Component - New way
+const Title = ()=>{
+    return <h1>This is title</h1>
+}
+const title = <h1>This is title React element</h1>
+
+const HeadingComponent = () => {
+    return (
+        <div>
+            <Title/>
+            {title}
+             <h1>This is functional component</h1>
+        </div>
+     )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
